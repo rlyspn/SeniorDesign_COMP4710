16be8f0de276673c2e5de86abdfb3a4bf0578072
Alexander Luchs <anl0005@Zeratul.local>
2012-01-23 17:44:32 -0600

    added calculateEnergy() test, Riely mad some changes to the makefile as well.

--- LinearMetropolis/test/linearTest.cpp
+++ LinearMetropolis/test/linearTest.cpp
@@ -27,10 +27,45 @@ void testWrapBox(){
 }
 
 void testCalculateEnergy(){
+    double x1 = 1.0;
+    double y1 = 1.0;
+    double z1 = 1.0;
 
+    double x2 = 2.0;
+    double y2 = 2.0;
+    double z2 = 2.0;
+
+    double **coords = new double*[2];
+    
+    coords[0] = new double[3];
+    coords[1] = new double[3];
+
+    coords[0][0] = x1;
+    coords[0][1] = y1;
+    coords[0][2] = z1;
+    coords[1][0] = x2;
+    coords[1][1] = y2;
+    coords[1][2] = z2;
+       
+    double sigma = 3.624;
+    double epsilon = 0.317;
+    double box_size[3] = {5.0, 5.0, 5.0};
+    double boxSize = 5.0;
+    
+    Atom *atoms = new Atom[2];
+    atoms[0] = createAtom(0, x1, y1, z1, sigma, epsilon);
+    atoms[1] = createAtom(1, x2, y2, z2, sigma, epsilon);
+    
+    double baseEnergy = calculate_energy(coords, 2, box_size, sigma, epsilon);
+    double testEnergy = calculateEnergy(atoms, 2, boxSize);
+    
+    assert(baseEnergy == testEnergy);
+    
+    printf("testCalculateEnergy succeeded\n");  
 }
 
 int main(){
     testMakePeriodic();
     testWrapBox();
+    testCalculateEnergy();
 }
