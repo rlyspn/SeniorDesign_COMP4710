1738cecd7cf15589a6615c074529ca8be9e85039
Riley Spahn <rileyspahn@gmail.com>
2012-01-26 16:17:00 -0600

    Added 1 in index calculation.

--- parallelMetropolis/src/metroCudaUtil.cu
+++ parallelMetropolis/src/metroCudaUtil.cu
@@ -4,14 +4,10 @@
 
 //calculates X (larger indexed atom) for energy calculation based on index in atom array
 __device__ int getXFromIndex(int idx){
-    if(idx == 0)
-        return 0;
-    else{
-        int c = -2 * idx;
-        int discriminant = 1 - 4 * c;
-        int qv = (-1 + sqrtf(discriminant)) / 2;
-        return qv;
-    }
+    int c = -2 * idx;
+    int discriminant = 1 - 4 * c;
+    int qv = (-1 + sqrtf(discriminant)) / 2;
+    return qv + 1;
 }
 
 //calculates Y (smaller indexed atom) for energy calculation based on index in atom array
