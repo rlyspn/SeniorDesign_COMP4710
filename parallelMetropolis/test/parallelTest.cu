848e6239f445c5a8555d735fb80f4d92c28ed266
Alexander Luchs <anl0005@Zeratul.local>
2012-01-30 15:26:16 -0600

    parallelTest should be fully fixed now.

--- parallelMetropolis/test/parallelTest.cu
+++ parallelMetropolis/test/parallelTest.cu
@@ -1,19 +1,38 @@
 #include "parallelTest.cuh"
 
-void setupGetXFromIndex(){
+/**
+    Wrapper function that will call the global function used to 
+    test the functions that calculate indexes in the half array
+    used to hold the energies.
+*/
+void setupGetIndexTest(){
     int numberOfBlocks = 3;
     int threadsPerBlock = 2;
+    int totalTests = numberOfBlocks * threadsPerBlock;
 
     int *xValues;
+    int *yValues;
+    int *yValues_device;
     int *xValues_device;
     
-    size_t xSize = numberOfBlocks * threadsPerBlock * sizeof(int);
+    size_t xSize = totalTests * sizeof(int);
+    
+    yValues = (int *) malloc(xSize);
     xValues = (int *)malloc(xSize);
+    cudaMalloc((void **) &yValues_device, xSize);
     cudaMalloc((void **) &xValues_device, xSize);
     
-    testGetXKernel <<<numberOfBlocks, threadsPerBlock>>>(xValues_device);
+    testGetXKernel <<<numberOfBlocks, threadsPerBlock>>>(xValues_device, totalTests);
 
     cudaMemcpy(xValues, xValues_device, xSize, cudaMemcpyDeviceToHost);
+
+    printf("Testing getting x index\n");
+    printf("%d\n", xValues[0]);
+    printf("%d\n", xValues[1]);
+    printf("%d\n", xValues[2]);
+    printf("%d\n", xValues[3]);
+    printf("%d\n", xValues[4]);
+    printf("%d\n", xValues[5]);
 
     assert(xValues[0] == 1);
     assert(xValues[1] == 2);
@@ -24,43 +43,144 @@ void setupGetXFromIndex(){
 
     printf("getXFromIndex Correct\n");
 
+    //test getYFromIndex)
+    testGetYKernel <<<numberOfBlocks, threadsPerBlock>>> (xValues_device,
+            yValues_device, totalTests);
+
+    cudaMemcpy(yValues, yValues_device, xSize, cudaMemcpyDeviceToHost);
+    
+    printf("Testing getting y index\n");
+    printf("%d\n", yValues[0]);
+    printf("%d\n", yValues[1]);
+    printf("%d\n", yValues[2]);
+    printf("%d\n", yValues[3]);
+    printf("%d\n", yValues[4]);
+    printf("%d\n", yValues[5]);
+
+
+    assert(yValues[0] == 0);
+    assert(yValues[1] == 0);
+    assert(yValues[2] == 1);
+    assert(yValues[3] == 0);
+    assert(yValues[4] == 1);
+    assert(yValues[5] == 2);
+
     cudaFree(xValues_device);
+    cudaFree(yValues_device);
+    free(yValues);
     free(xValues);
 }
-__global__ void testGetXKernel(int *xValues){
-    int idx =  threadIdx.x + blockIdx.x * blockDim.x;
-    
-    //xValues[idx] = getXFromIndex(idx); 
-}
 
-void setupGetYFromIndex(){
-    //TODO
-}
-__global__ void testGetYKernel(){
-    //TODO
-}
 
+
+/**
+  wrapper function for the __device__ makePeriodic function
+*/
 void setupMakePeriodic(){
-    //TODO
+    srand(time(NULL));
+    int numberOfTests = 128;
+    double *box;;
+    
+    double *inputs_host;
+    double *inputs_device;
+    double *outputs_host;
+    double *dev_box;
+    size_t inputSize = sizeof(double) * numberOfTests;
+
+    box = (double *) malloc(sizeof(double));
+    *box = 10.0;
+    inputs_host = (double *) malloc(inputSize);
+    outputs_host = (double *) malloc(inputSize);
+    cudaMalloc((void **) &inputs_device, inputSize);
+    cudaMalloc((void **) &dev_box, sizeof(double));
+    
+    //generate random numbers
+    for(int i = 0; i < numberOfTests; i++){
+        inputs_host[i] = ((double) rand());
+    }
+
+    //copy data to device
+    cudaMemcpy(inputs_device, inputs_host, inputSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_box, box, sizeof(double), cudaMemcpyHostToDevice);
+    
+    int threadsPerBlock = numberOfTests / 2;
+    int blocks = numberOfTests / threadsPerBlock +
+        (numberOfTests % threadsPerBlock == 0 ? 0 : 1);
+
+    testMakePeriodicKernel <<< blocks, threadsPerBlock >>> (inputs_device,
+            dev_box, numberOfTests);
+
+    cudaMemcpy(outputs_host, inputs_device, inputSize, cudaMemcpyDeviceToHost);
+
+    //check that values are the same as known correct function
+    for(int i = 0; i < numberOfTests; i++){
+        double test_output = make_periodic(inputs_host[i], *box);
+        
+        printf("inputs_host[%d] = %f | outputs_host[%d] = %f | test_output = %f\n", i, inputs_host[i], i, outputs_host[i], test_output);
+        assert(outputs_host[i] == test_output);
+    }
+
+    printf("makePeriodic passed Tests\n");
+
+    free(inputs_host);
+    free(outputs_host);
+    cudaFree(inputs_device);
+
+
 }
 
-__global__ void testMakePeriodicKernel(){
-    //TODO
-}
+
 
 void setupWrapBox(){
-    //TODO
-}
-__global__ void testWrapBoxKernel(){
-    //TODO    
+    srand(time(NULL));
+    int numberOfTests = 128;
+    double box = 10;
+    
+    double *inputs_host;
+    double *inputs_device;
+    double *outputs_host;
+    size_t inputSize = sizeof(double) * numberOfTests;
+
+    inputs_host = (double *) malloc(inputSize);
+    outputs_host = (double *) malloc(inputSize);
+    cudaMalloc((void  **) &inputs_device, inputSize);
+    
+    //generate random numbers
+    for(int i = 0; i < numberOfTests; i++){
+        inputs_host[i] = ((double) rand());
+    }
+
+    //copy data to device
+    cudaMemcpy(inputs_device, inputs_host, inputSize, cudaMemcpyHostToDevice);
+    
+    int threadsPerBlock = numberOfTests / 2;
+    int blocks = numberOfTests / threadsPerBlock +
+        (numberOfTests % threadsPerBlock == 0 ? 0 : 1);
+
+    //run test on device
+    testWrapBoxKernel <<<blocks, threadsPerBlock>>> (inputs_device, &box, numberOfTests);
+
+    cudaMemcpy(outputs_host, inputs_device, inputSize, cudaMemcpyDeviceToHost);
+
+    //check that values are the same as known correct function
+    for(int i = 0; i < numberOfTests; i++){
+        double test_output = wrap_into_box(inputs_host[i], box);
+        printf("inputs_host[%d] = %f | outputs_host[%d] = %f | test_output = %f\n", i, inputs_host[i], i, outputs_host[i], test_output);
+        assert(outputs_host[i] == test_output);
+    }
+
+    free(inputs_host);
+    free(outputs_host);
+    cudaFree(inputs_device);
+
+    printf("__device__ wrapBox tested correctly\n");
+
 }
 
 void setupCalc_lj(){
     //TODO
 }
-__global__ void testCalcLJKernel(){
-    //TODO
-}
+
 
 void testGeneratePoints(){
     //init atoms, environment
@@ -73,48 +193,24 @@ void testGeneratePoints(){
 
     Environment *enviro = &stableEnviro;
 
-    //calculate size of atoms and environemnt structs
-    size_t atomsSize = sizeof(*atoms);
-    size_t enviroSize = sizeof(*enviro);
+    generatePoints(atoms, enviro);
 
-    //declare device structs
-    Atom *dev_atoms = new Atom[numberOfAtoms];
-    Environment *dev_enviro;
-
-    //allocate memory for device structs
-    cudaMalloc( (void**) &dev_atoms, atomsSize);
-    cudaMalloc( (void**) &dev_enviro, enviroSize);
-
-    //copy local structs to device structs on device
-    cudaMemcpy(dev_atoms, atoms, atomsSize, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_enviro, enviro, enviroSize, cudaMemcpyHostToDevice);
-
-    //allocate memory on device for random number generator state
-    curandState* devStates;
-    cudaMalloc ( &devStates, numberOfAtoms*sizeof( curandState ) );
-                
-    // setup seeds
-    setup_generator <<<5, 2>>> ( devStates, time(NULL) );
-
-    // generate random numbers
-    generatePoints <<<5, 2>>> ( devStates, dev_atoms, dev_enviro );
-
-    //copy atoms back to host
-    cudaMemcpy(atoms, dev_atoms, atomsSize, cudaMemcpyDeviceToHost);
+    printf("Box limits{x:%f, y:%f, z:%f}\n", enviro->x, enviro->y, enviro->z);
 
     //assert that all atoms positions are in range of the box
     for (int i = 0; i < numberOfAtoms; i++){
         double dim_x = atoms[i].x;
         double dim_y = atoms[i].y;
         double dim_z = atoms[i].z;
+
         
-        printf("%f, %f, %f\n", dim_x, dim_y, dim_z);
+        printf("Atom %d{x:%f, y:%f, z:%f}\n", i, dim_x, dim_y, dim_z);
 
         assert(dim_x >= 0.0 && dim_x <= enviro->x &&
                dim_y >= 0.0 && dim_y <= enviro->y &&
                dim_z >= 0.0 && dim_z <= enviro->z);
     }
-    printf("testGeneratePoints successful.");
+    printf("testGeneratePoints successful.\n");
 }
 
 void testCalcEnergy(){
@@ -181,7 +277,9 @@ void testCalcEnergy(){
 }
 
 int main(){
-    //testGeneratePoints(); 
-	 testCalcEnergy();   
+    setupGetIndexTest();
+    setupMakePeriodic();
+    setupWrapBox();
+    testGeneratePoints();
     return 0;
 }
