8274730c1d5adfc302f72f745f6a5c5b7a2058ff
Alexander Luchs <anl0005@Zeratul.local>
2012-01-30 22:34:36 -0600

    minor changes to metroCudaUtil.cu

--- parallelMetropolis/src/metroCudaUtil.cu
+++ parallelMetropolis/src/metroCudaUtil.cu
@@ -176,6 +176,7 @@ double calcEnergyWrapper(Atom *atoms, Environment *enviro){
     //cleanup
     cudaFree(atoms_device);
     cudaFree(energySum_device);
+    cudaFree(enviro_device);
     free(energySum_host);
 
     return totalEnergy;
