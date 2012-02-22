199930b4f9ae8239ffc4aa9c3086cd28dd66fa29
Alexander Luchs <anl0005@Zeratul.local>
2012-02-22 00:45:20 -0600

    First draft of config scanner is up.  It kinda sucks.  I want to go over it with somebody.  It compiles but I'm not confident in it's quality or how it fits into our design at the moment.  I have a lot of questions.

---
+++ Utilities/src/Config_Scan.h
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include "metroUtil.h"
+#include <cstdlib>
+
+using namespace std;
+
+class Config_Scan{
+    private:
+        Environment* enviro;
+        string configpath;
+        long numOfSteps;
+        string oplsuaparPath;
+        string zmatrixPath;
+        string statePath;
+        string stateOutputPath;
+        string pdbOutputPath;
+
+    public:
+
+        /*
+            Config file scanner instantiated with a path.
+            @param configPath - path to paramaters.cfg
+        */
+        Config_Scan(string configPath);
+
+        /*
+            Reads in the config file located at config path
+            given in teh constructor.
+        */
+        void readInConfig();
+
+        /*
+            getters
+        */
+
+        Environment* getEnviro();
+
+        string getConfigPath();
+
+        long getSteps();
+        
+        string getOplsusaparPath();
+
+        string getZmatrixPath();
+
+        string getStatePath();
+
+        string getStateOutputPath();
+
+        string getPdbOutputPath();
+
+};
